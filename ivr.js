1df1545766d7599fce5f78bd6976dd518598d713
Calen Pennington <cale@edx.org>
2017-06-08 11:14:04 -0400

    Add javascript code

---
+++ ivr.js
@@ -0,0 +1,9 @@
+// Retrieve the utils library
+var utils = window.optimizely.get('utils');
+
+// Wait for the upgrade link element to appear in the DOM, then change the color
+utils.waitForElement('.upgrade-link').then(function(upgradeLink) {
+    var upgradeUser = document.getElementById('upgrade_user');
+    upgradeLink.href = upgradeUser.dataset.link;
+    upgradeLink.childNodes[1].childNodes[1].innerHTML += " (" + upgradeUser.dataset.price + ")";
+});
