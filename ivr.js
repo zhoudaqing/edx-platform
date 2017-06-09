42c3c62e2f160c76c94018b823a09fe0202aef0d
Gabe Mulley <gabe@edx.org>
2017-06-09 06:26:57 -0400

    move code to the shared snippets

--- ivr.js
+++ ivr.js
@@ -1,13 +1,3 @@
-// Retrieve the utils library
-var utils = window.optimizely.get('utils');
-
-// Wait for the upgrade link element to appear in the DOM, then change the color
-utils.waitForElement('.upgrade-link').then(function(upgradeLink) {
-    var upgradeUser = document.getElementById('upgrade_user');
-    upgradeLink.href = upgradeUser.dataset.link;
-    upgradeLink.innerHTML += " (" + upgradeUser.dataset.price + ")";
-});
-
 $(function () {
   var STATUS_KEY = 'ret.verificationHeroExpanded',
     $verificationHeroOuter = $('.verification-hero-wrapper'),
