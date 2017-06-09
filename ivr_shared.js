13eacbe90fa76e1d2027885da0b264a92580fea3
Gabe Mulley <gabe@edx.org>
2017-06-09 07:42:29 -0400

    dynamic element insertion

--- ivr_shared.js
+++
@@ -1,9 +0,0 @@
-// Retrieve the utils library
-var utils = window.optimizely.get('utils');
-
-// Wait for the upgrade link element to appear in the DOM, then change the color
-utils.waitForElement('.upgrade-link').then(function(upgradeLink) {
-    var upgradeUser = document.getElementById('upgrade_user');
-    upgradeLink.href = upgradeUser.dataset.link;
-    upgradeLink.innerHTML += " (" + upgradeUser.dataset.price + ")";
-});
