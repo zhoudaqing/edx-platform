572770d7fa4b98e33525212d4a6d1c24ce318c34
Gregory Martin <greg@edx.org>
2017-06-22 15:01:03 -0400

    add cert task firing on whitelist append

--- lms/djangoapps/certificates/tests/test_signals.py
+++ lms/djangoapps/certificates/tests/test_signals.py
@@ -2,12 +2,15 @@
 Unit tests for enabling self-generated certificates for self-paced courses
 and disabling for instructor-paced courses.
 """
+import mock
+
 from certificates import api as certs_api
-from certificates.models import CertificateGenerationConfiguration
+from certificates.models import CertificateGenerationConfiguration, CertificateWhitelist
 from certificates.signals import _listen_for_course_pacing_changed
 from openedx.core.djangoapps.self_paced.models import SelfPacedConfiguration
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 from xmodule.modulestore.tests.factories import CourseFactory
+from student.tests.factories import UserFactory
 
 
 class SelfGeneratedCertsSignalTest(ModuleStoreTestCase):
@@ -40,3 +43,32 @@ class SelfGeneratedCertsSignalTest(ModuleStoreTestCase):
         _listen_for_course_pacing_changed('store', self.course.id, self.course.self_paced)
         # verify that self-generation of cert is disabled for instructor-paced course
         self.assertFalse(certs_api.cert_generation_enabled(self.course.id))
+
+
+class WhitelistGeneratedCertificatesTest(ModuleStoreTestCase):
+    """
+    Tests for enabling/disabling self-generated certificates according to course-pacing.
+    """
+    def setUp(self):
+        super(WhitelistGeneratedCertificatesTest, self).setUp()
+        self.course = CourseFactory.create(self_paced=True)
+        self.user = UserFactory.create()
+        SelfPacedConfiguration(enabled=True).save()
+
+    def test_cert_generation_on_whitelist_append(self):
+        """
+        Verify that signal enables or disables self-generated certificates
+        according to course-pacing.
+        """
+        with mock.patch(
+                'lms.djangoapps.certificates.signals.generate_certificate.apply_async',
+                return_value=None
+        ) as mock_generate_certificate_apply_async:
+            CertificateWhitelist.objects.create(
+                user=self.user,
+                course_id=self.course.id
+            )
+            mock_generate_certificate_apply_async.assert_called_once_with(
+                student=self.user,
+                course_key=self.course.id,
+            )
