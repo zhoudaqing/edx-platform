93980ebb403decba187521a877e81a830f45420c
Kyle McCormick <kmccormick@edx.org>
2017-06-22 19:24:14 -0400

    Improve process_event method

--- lms/djangoapps/django_comment_client/base/event_transformers.py
+++ lms/djangoapps/django_comment_client/base/event_transformers.py
@@ -9,6 +9,7 @@ from opaque_keys.edx.locator import CourseLocator
 
 from django_comment_client.utils import get_cached_discussion_id_map_by_course_id
 from django_comment_client.base.views import add_truncated_title_to_event_data
+from teams.models import CourseTeam
 from track.transformers import EventTransformer, EventTransformerRegistry
 
 
@@ -23,6 +24,20 @@ class ForumThreadViewedEventTransformer(EventTransformer):
     match_key = u'edx.forum.thread.viewed'
 
     def process_event(self):
+        """
+        Transform incoming events.
+
+        For events from mobile, enhance with fields that are not available
+        within the apps, and remove unncessary fields.
+
+        Pass-through other events.
+        """
+
+        # Do not transform non-mobile events; they should already have all the correct fields
+        if self['event_source'] != 'mobile':
+            return
+
+        # Parse out course key; extract topic and thread IDs
         course_id_string = self.event.get('course_id')
         course_id = None
         if course_id_string:
@@ -30,10 +45,10 @@ class ForumThreadViewedEventTransformer(EventTransformer):
                 course_id = CourseLocator.from_string(course_id_string)
             except InvalidKeyError:
                 pass
-
         commentable_id = self.event.get('commentable_id')
         thread_id = self.event.get('id')
 
+        # Load user
         username = self.get('username')
         user = None
         if username:
@@ -42,12 +57,14 @@ class ForumThreadViewedEventTransformer(EventTransformer):
             except User.DoesNotExist:
                 pass
 
+        # If in a category, add category name and ID
         if course_id and commentable_id and user:
             id_map = get_cached_discussion_id_map_by_course_id(course_id, [commentable_id], user)
             if commentable_id in id_map:
                 self.event['category_name'] = id_map[commentable_id]['title']
                 self.event['category_id'] = commentable_id
 
+        # Add thread URL
         if course_id and commentable_id and thread_id:
             url_kwargs = {
                 'course_id': course_id_string,
@@ -56,6 +73,10 @@ class ForumThreadViewedEventTransformer(EventTransformer):
             }
             self.event['url'] = reverse('single_thread', kwargs=url_kwargs)
 
+        # Add truncated title
+        add_truncated_title_to_event_data(self.event, self.event.get('title'))
+
+        # Add user's forum and course roles
         if course_id and user:
             self.event['user_forums_roles'] = [
                 role.name for role in user.roles.filter(course_id=course_id)
@@ -64,7 +85,18 @@ class ForumThreadViewedEventTransformer(EventTransformer):
                 role.role for role in user.courseaccessrole_set.filter(course_id=course_id)
             ]
 
-    	add_truncated_title_to_event_data(self.event, self.event.get('title'))
+        # Add team ID
+        if commentable_id:
+            try:
+                team = CourseTeam.objects.get(discussion_topic_id=commentable_id)
+                self.event['team_id'] = team.team_id
+            except CourseTeam.DoesNotExist:
+                pass
 
+        # Remove course ID field.
+        # It is in the mobile event data because it is necessary in order to calculate the
+        # user_forum_roles and user_course_roles fields; however, it is not required in
+        # the final event.
+        it is not needed in the final event.
         if 'course_id' in self.event:
-        	del self.event['course_id']
+            del self.event['course_id']
